f4289aa0da10b61e3c454443841e1cfe29a62235
nguyennd <nguyennd92.dev@gmail.com>
2025-12-20 07:46:29 +0700

    add test

--- src/test/java/top/nguyennd/expense/AbstractControllerIT.java
+++ src/test/java/top/nguyennd/expense/AbstractControllerIT.java
@@ -1,15 +1,9 @@
 package top.nguyennd.expense;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
-class ExpenseApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
+public class AbstractControllerIT {
 }
